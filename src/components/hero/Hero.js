ed393a1d0f65cd753dcff8e29cabc3e410a3b5fa
PHAGUN-JAIN <phagunjain@protonmail.com>
2022-04-01 23:59:18 +0530

    hero section

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,31 @@
+import "./Hero.css";
+import logo from "../../assets/logo.svg";
+export default function Hero() {
+  return (
+    <>
+      <div class="container-max-width">
+        <div class="columns">
+          <div class="column left-panel">
+            <p>
+              First column <br />
+              United Club Of Developers Is A Tech Oriented Student Chapter From
+              UPES, Dheradun
+            </p>
+            <br />
+            <div class="cta-wrapper">
+              <a href="#" id="cta1">
+                Register Now
+              </a>
+              <a href="#" id="cta2">
+                Join Community
+              </a>
+            </div>
+          </div>
+          <div class="column right-panel">
+            <img src={logo} alt="logo" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,12 @@
 import "bulma/css/bulma.css";
-
+import ucdLogo from "../../assets/Vector.svg";
 export default function Navbar() {
   return (
     <>
-      <nav class="navbar" role="navigation" aria-label="main navigation">
+      <nav class="navbar" role="navigation" aria-label="main navigation" >
         <div class="navbar-brand">
           <a class="navbar-item" href="https://bulma.io">
-            <img
-              src="https://bulma.io/images/bulma-logo.png"
-              width="112"
-              height="28"
-            />
+            <img src={ucdLogo} width="112" height="28" />
           </a>
 
           <a
